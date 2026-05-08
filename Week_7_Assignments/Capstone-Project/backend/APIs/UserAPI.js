776525ad98723483fb2524aa4ec35ec4db1907f0
Bandari Sushanth <sushanth1208@gmail.com>
2026-05-08 09:56:52 +0530

    Updated UserAPI.js

--- Week_7_Assignments/Capstone-Project/backend/APIs/UserAPI.js
+++ Week_7_Assignments/Capstone-Project/backend/APIs/UserAPI.js
@@ -1,43 +1,83 @@
-import exp from 'express';
-import { verifyToken } from '../middlewares/VerifyToken.js';
-import { ArticleModel } from '../models/ArticleModel.js';
+import exp from "express";
+import { VerifyToken } from "../middlewares/VerifyToken.js";
+import { ArticleModel } from "../models/ArticleModel.js";
 
-export const userApp=exp.Router();
+export const userApp = exp.Router();
 
-//Read Articles of all authors
-userApp.get("/articles",verifyToken("USER"),async(req,res)=>{
-    //read articles
-    const articlesList=await ArticleModel.find({isArticleActive: true}).populate("comments.user");
-    //send res
-    res.status(200).json({message:"All Articles",payload:articlesList})
+// Read Articles of all authors
+userApp.get(
+  "/articles",
+  VerifyToken("USER"),
+  async (req, res) => {
+    try {
+      // read articles
+      const articlesList = await ArticleModel.find({
+        isArticleActive: true,
+      }).populate("comments.user");
 
-})
-
-//Add comment to an article
-userApp.put("/article",verifyToken("USER"),async(req,res)=>{
-    //get body from req
-    const {articleId,comment}=req.body;
-
-    //check article
-    const articleDocument=await ArticleModel.findOne({_id:articleId,isArticleActive: true}).populate("comments.user");
-    
-    //if article not found
-    if(!articleDocument){
-        return res.status(404).json({message:"Article not found"})
+      // send response
+      res.status(200).json({
+        message: "All Articles",
+        payload: articlesList,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "Error fetching articles",
+        error: err.message,
+      });
     }
-    //get user id from the Token
-    const userIdOfToken=req.user?.id;
+  }
+);
 
-    //add comment to comments array of article document
-    articleDocument.comments.push({user:userIdOfToken,comment:comment})
+// Add comment to an article
+userApp.put(
+  "/article",
+  VerifyToken("USER"),
+  async (req, res) => {
+    try {
+      // get body from request
+      const { articleId, comment } = req.body;
 
-    //save
-    await articleDocument.save();
+      // check article
+      const articleDocument = await ArticleModel.findOne({
+        _id: articleId,
+        isArticleActive: true,
+      }).populate("comments.user");
 
-    const updatedArticle = await ArticleModel.findById(articleId).populate("comments.user");
+      // if article not found
+      if (!articleDocument) {
+        return res.status(404).json({
+          message: "Article not found",
+        });
+      }
 
-    //send res
-    res.status(200).json({message:"Comment added successfully", payload:updatedArticle})
-})
+      // get user id from token
+      const userIdOfToken = req.user?.id;
 
+      // add comment
+      articleDocument.comments.push({
+        user: userIdOfToken,
+        comment: comment,
+      });
 
+      // save article
+      await articleDocument.save();
+
+      // get updated article
+      const updatedArticle = await ArticleModel.findById(
+        articleId
+      ).populate("comments.user");
+
+      // send response
+      res.status(200).json({
+        message: "Comment added successfully",
+        payload: updatedArticle,
+      });
+    } catch (err) {
+      res.status(500).json({
+        message: "Error adding comment",
+        error: err.message,
+      });
+    }
+  }
+);
