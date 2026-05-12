29713f88942d716a9caa25a0c8855cc763ce6f15
Bandari Sushanth <sushanth1208@gmail.com>
2026-05-12 09:20:13 +0530

    Refactor user registration in Register component
    Refactor Register component to simplify user registration process. Removed file upload handling and replaced it with direct user object submission.

--- Week_7_Assignments/Capstone-Project/frontend/src/components/Register.jsx
+++ Week_7_Assignments/Capstone-Project/frontend/src/components/Register.jsx
@@ -9,13 +9,11 @@ import {
   pageBackground,
   submitBtn,
   mutedText,
-  loadingClass
-} from "../styles/common";
+} from "../styles/common.js";
 import { useForm } from "react-hook-form";
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import { useState } from "react";
-import { useNavigate } from "react-router";
-import axios from 'axios';
+import api from "../api/axiosInstance.js"
 
 function Register() {
   const {
@@ -25,51 +23,33 @@ function Register() {
   } = useForm();
   const [loading, setLoading] = useState(false);
   const [apiError, setApiError] = useState(null);
-  const [preview,setPreview]=useState(null);
-  const navigate=useNavigate();
+  const navigate=useNavigate()
 
   //When user registration submitted
   const onUserRegister = async (userObj) => {
     console.log(userObj);
-    let {profileImageUrl}=userObj;
-    //file + userObj ---> FormData
-    //create a FormData Object
-    const formData=new FormData();
-    //add all user properties and file to this formData Object
-    formData.append("firstName",userObj.firstName)
-    //append if lastName is existed
-    if(userObj.lastName)
-    {
-      formData.append("lastName",userObj.lastName)
-    }
-    formData.append("email",userObj.email)
-    formData.append("password",userObj.password)
-    formData.append("role", userObj.role);
-    //append if image is existed
-    if(profileImageUrl?.[0]) 
-      {
-        formData.append("profileImageUrl", profileImageUrl[0]);
-      }
-
-
     try {
-      //start loading
-      setLoading(true);
-      let res=await axios.post("https://atp-24eg105q31.onrender.com",formData,{withCredentials:true})
-      if(res.status===201)
-      {
-        //navigate to Login
-        navigate("/login");
+      //Start loading
+      setLoading(true)
+      //Make HTTP POST request to create user in backend
+      let res=await api.post("/common-api/users",userObj)
+      if(res.status===201){
+        navigate("/login")
       }
-    } catch (apiError) {
-      console.log("err in registration", apiError);
-      setApiError(apiError.response?.data?.error || "Registration failed");
+      //Navigate to login
+    } catch (err) {
+      console.log("err in registration", err);
+      setApiError(err.response?.data?.error || "Registration failed");
     } finally {
       setLoading(false);
     }
   };
-
-  if (loading) return <p className={loadingClass}>Registering...</p>;
+  if(loading){
+    return <p className="text-3xl text-center">Loading...</p>
+  }
+  if(apiError){
+    return <p className="text-red-500">{apiError}</p>
+  }
 
   return (
     <div className={`${pageBackground} flex items-center justify-center py-16 px-4`}>
@@ -77,7 +57,7 @@ function Register() {
         <h2 className={formTitle}>Create an Account</h2>
 
         {/* API Error */}
-        {apiError && <p className={errorClass}>{apiError}</p>}
+        {/* {apiError && <p className={errorClass}>{apiError}</p>} */}
 
         <form onSubmit={handleSubmit(onUserRegister)}>
           {/* ROLE */}
@@ -165,6 +145,7 @@ function Register() {
               placeholder="you@example.com"
               {...register("email", {
                 required: "Email is required",
+                // required: [true, "Password is required"],
               })}
             />
             {errors.email && <p className={errorClass}>{errors.email.message}</p>}
@@ -184,42 +165,14 @@ function Register() {
             {errors.password && <p className={errorClass}>{errors.password.message}</p>}
           </div>
 
-          {/* PROFILE IMAGE */}
+          {/* PROFILE IMAGE
           <div className={formGroup}>
             <label className={labelClass}>Profile Image</label>
 
-            <input type="file" 
-            className={inputClass} 
-            accept="image/png, image/jpeg" 
-            {...register("profileImageUrl",{
-              validate:{
-              fileType:(files)=>{
-                if(!files?.[0]) return true
-                return ["image/png", "image/jpeg"].includes(files[0].type) || "Only JPEG or PNG is allowed"
-              },
-              fileSize:(files)=>{
-                if(!files?.[0]) return true
-                return files[0].size<=2*1024*1024 ||"Max Size is 2MB"
-              }
-              }
-            })} 
-            onChange={(event)=>{
-              let file=event.target.files[0]
-              if(file){
-                setPreview(URL.createObjectURL(file));
-              }
-            }}
-            />
+            <input type="text" accept="image/png, image/jpeg" {...register("profileImageUrl")} />
 
             {errors.profileImageUrl && <p className={errorClass}>{errors.profileImageUrl.message}</p>}
-            {/* Image Preview */}
-
-            {
-              preview && <div>
-                <img src={preview} alt="" className="w-24 "/>  
-              </div>
-            }
-          </div>
+          </div> */}
 
           {/* SUBMIT */}
           <button type="submit" className={submitBtn}>
