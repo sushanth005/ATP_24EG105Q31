f2c5b535935deb4fcc9af12fa94d822c008db03d
Bandari Sushanth <sushanth1208@gmail.com>
2026-05-12 09:18:31 +0530

    Update Footer.jsx

--- Week_7_Assignments/Capstone-Project/frontend/src/components/Footer.jsx
+++ Week_7_Assignments/Capstone-Project/frontend/src/components/Footer.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="bg-gray-800 text-white p-4 mt-8">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Blog App. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
