3aa60c9f0ebcab199af08f46fe7a0a08051b99b3
Bandari Sushanth <sushanth1208@gmail.com>
2026-05-12 13:53:25 +0530

    Fixed

--- Week_7_Assignments/Capstone-Project/backend/APIs/AuthorAPI.js
+++ Week_7_Assignments/Capstone-Project/backend/APIs/AuthorAPI.js
@@ -1,7 +1,7 @@
 import exp from 'express';
 import {UserModel} from '../models/UserModel.js';
 import {ArticleModel} from '../models/ArticleModel.js';
-import { VerifyToken } from '../middlewares/VerifyToken.js';
+import { verifyToken } from '../middlewares/VerifyToken.js';
 
 export const authorApp=exp.Router();
 
