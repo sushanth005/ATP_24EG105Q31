d788a5a3f01184ec02d952cf7ac57cab07a27c95
Bandari Sushanth <sushanth1208@gmail.com>
2026-05-13 12:15:55 +0530

    Refactor App component and update imports

--- Week_7_Assignments/Capstone-Project/frontend/src/App.jsx
+++ Week_7_Assignments/Capstone-Project/frontend/src/App.jsx
@@ -6,12 +6,12 @@ import Login from "./components/Login";
 import UserProfile from "./components/UserProfile";
 import AuthorProfile from "./components/AuthorProfile";
 import AuthorArticles from "./components/AuthorArticles";
-import EditArticle from './components/EditArticle'
+import EditArticle from "./components/EditArticle";
 import WriteArticles from "./components/WriteArticles";
 import ArticleByID from "./components/ArticleByID";
-import {Toaster} from 'react-hot-toast';
-import ProtectedRoute from "./components/ProtectedRoutes";
-
+import { Toaster } from "react-hot-toast";
+import Unauthorized from "./components/Unauthorized";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const routerObj = createBrowserRouter([
@@ -33,15 +33,19 @@ function App() {
         },
         {
           path: "user-profile",
-          element: <ProtectedRoute allowedRoles={["USER"]}>
-            <UserProfile />
-          </ProtectedRoute>,
+          element: (
+            <ProtectedRoute allowedRoles={["USER"]}>
+              <UserProfile />
+            </ProtectedRoute>
+          ),
         },
         {
           path: "author-profile",
-          element: <ProtectedRoute allowedRoles={["AUTHOR"]}>
-            <AuthorProfile/>
-          </ProtectedRoute>,
+          element: (
+            <ProtectedRoute allowedRoles={["AUTHOR"]}>
+              <AuthorProfile />
+            </ProtectedRoute>
+          ),
 
           children: [
             {
@@ -66,72 +70,20 @@ function App() {
           path: "edit-article",
           element: <EditArticle />,
         },
+        {
+          path: "unauthorized",
+          element: <Unauthorized />,
+        },
       ],
     },
   ]);
 
   return (
     <div>
-      <Toaster pattern="top-order" reverseOrder={false}/>
+      <Toaster position="top-center" reverseOrder={false} />
       <RouterProvider router={routerObj} />
-  </div>
-);
+    </div>
+  );
 }
 
 export default App;
-
-
-
-
-/*import { createBrowserRouter,RouterProvider } from "react-router"
-import RootLayout from "./components/RootLayout"
-import Home from "./components/Home"
-import Register from "./components/Register"
-import Login from "./components/Login"
-import UserProfile from "./components/UserProfile"
-import AuthorProfile from "./components/AuthorProfile"
-import AdminProfile from "./components/AdminProfile"
-
-function App() {
-
-  const routerObj=createBrowserRouter([
-    {
-      path:"/",
-      element:<RootLayout/>,
-      children:[{
-        path:"",
-        element:<Home/>
-      },
-      {
-        path:"register",
-        element:<Register/>
-      },
-      {
-        path:"login",
-        element:<Login/>
-      },
-      {
-        path:"user-profile",
-        element:<UserProfile/>
-      },
-      {
-        path:"author-profile",
-        element:<AuthorProfile/>
-      },
-      {
-        path:"admin-profile",
-        element:<AdminProfile/>
-      }
-    
-    ]
-    }
-  ]
-  )
-  return (
-    <div>
-      <RouterProvider router={routerObj}/>
-    </div>
-  )
-}
-
-export default App */
