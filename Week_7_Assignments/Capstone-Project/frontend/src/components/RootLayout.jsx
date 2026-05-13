b96c564a29806cd9cf89c3133ef375ac7aec9fae
Bandari Sushanth <sushanth1208@gmail.com>
2026-05-13 12:25:19 +0530

    Refactor RootLayout for loading state and layout changes
    Refactor RootLayout component to include loading state and adjust layout styles.

--- Week_7_Assignments/Capstone-Project/frontend/src/components/RootLayout.jsx
+++ Week_7_Assignments/Capstone-Project/frontend/src/components/RootLayout.jsx
@@ -1,27 +1,30 @@
-import Header from "./Header"
-import Footer from "./Footer"
-import { Outlet } from "react-router"
-import { useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Outlet } from "react-router";
 import { useAuth } from "../store/authStore";
-
+import { useEffect } from "react";
 function RootLayout() {
-  //import checkAuth Function
-  let checkAuth=useAuth(state=>state.checkAuth)
 
-  useEffect(()=>{
-    checkAuth()
-  },[])
+  const checkAuth = useAuth((state) => state.checkAuth);
+  const loading = useAuth((state) => state.loading);
+
+  useEffect(() => {
+    checkAuth(); 
+  }, [checkAuth]);
+
+  if (loading) {
+    return <p>Loading app...</p>;
+  }
 
   return (
     <div>
-        <Header/>
-
-        <div className="min-h-screen mx-2">
-            <Outlet/>
-        </div>
-        <Footer/>
+      <Header />
+      <div className='min-h-screen mx-32'>
+        <Outlet />
+      </div>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default RootLayout
+export default RootLayout;
